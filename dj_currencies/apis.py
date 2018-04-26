8367e72e6f1c0424f2c9fbb5308cd34601d31cf0
Lihan Li <lihan@brightwrite.com>
2018-04-26 12:09:53 +1000

    convert_money should return..

--- dj_currencies/apis.py
+++ dj_currencies/apis.py
@@ -3,4 +3,5 @@ from .settings import currency_settings
 
 def convert_money(amount, currency_from, currency_to):
     backend = currency_settings.DEFAULT_BACKEND()
-    backend.convert_money(amount, currency_from, currency_to)
+    return backend.convert_money(amount, currency_from, currency_to)
+
