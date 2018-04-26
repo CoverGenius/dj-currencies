2e487fc027eaf38a4e2a2fc35d9a4148028ca451
Lihan Li <lihan@brightwrite.com>
2018-04-26 12:10:24 +1000

    Added new API for retrieving latest rates

--- dj_currencies/apis.py
+++ dj_currencies/apis.py
@@ -5,3 +5,7 @@ def convert_money(amount, currency_from, currency_to):
     backend = currency_settings.DEFAULT_BACKEND()
     return backend.convert_money(amount, currency_from, currency_to)
 
+
+def get_latest_cached_rates(symbols=None):
+    backend = currency_settings.DEFAULT_BACKEND()
+    return backend.get_cached_rates(symbols)
